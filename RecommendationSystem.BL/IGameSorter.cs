01b5a20e19e4f6e58f1260c56151f3eb5a20f243
Dmitry Orlov <nphoxconnect@gmail.com>
2018-04-08 17:59:18 +0300

    add IGameSorter

---
+++ RecommendationSystem.BL/IGameSorter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecommendationSystem.Data;
+
+namespace RecommendationSystem.BL
+{
+    interface IGameSorter
+    {
+        void QuickSort(double[] coefs, Game[] games, int left, int right);
+    }
+}
