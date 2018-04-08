fe32e7db2c2926b20e8be6d5f29113046dd3b3ee
Dmitry Orlov <nphoxconnect@gmail.com>
2018-04-08 18:00:48 +0300

    add GameSorter

---
+++ RecommendationSystem.BL/GameSorter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecommendationSystem.Data;
+
+namespace RecommendationSystem.BL
+{
+    public class GameSorter : IGameSorter
+    {
+        public void QuickSort(double[] coefs, Game[] games, int left, int right)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
