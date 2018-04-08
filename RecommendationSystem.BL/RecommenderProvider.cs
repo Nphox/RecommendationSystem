5e7b45fb4510753037a8bcc91a35a32766a676de
Dmitry Orlov <nphoxconnect@gmail.com>
2018-04-08 16:14:12 +0300

    add RecommendationProvider

---
+++ RecommendationSystem.BL/RecommenderProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecommendationSystem.Data;
+
+namespace RecommendationSystem.BL
+{
+    public class RecommenderProvider : IRecommendationProvider
+    {
+        public Game[] RecommendGames(Game[] games, Game templateGame, int numberSimilarGames)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
