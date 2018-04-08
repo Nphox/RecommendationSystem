5f87b9e85ae8bf4a527593bd806215736bf52914
Dmitry Orlov <nphoxconnect@gmail.com>
2018-04-08 17:46:54 +0300

    add ICalculateSimilarity

---
+++ RecommendationSystem.BL/ISimilarityCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecommendationSystem.Data;
+
+namespace RecommendationSystem.BL
+{
+    public interface ISimilarityCalculator
+    {
+        double CalculateSimilarity(Game checkGame, Game templateGame);
+    }
+}
