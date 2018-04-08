8f29005d09f145bbd03db04fb3ba10d636955808
Dmitry Orlov <nphoxconnect@gmail.com>
2018-04-08 18:06:49 +0300

    GameSorter implemented

--- RecommendationSystem.BL/GameSorter.cs
+++ RecommendationSystem.BL/GameSorter.cs
@@ -9,9 +9,44 @@ namespace RecommendationSystem.BL
 {
     public class GameSorter : IGameSorter
     {
+        private void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        private int Partition(double[] koefs, Game[] games, int left, int right)
+        {
+            double pivot = koefs[(left + right) / 2];
+            int i = left;
+            int j = right;
+
+            while (i <= j)
+            {
+                while (koefs[i] < pivot) { i++; }
+                while (koefs[j] > pivot) { j--; }
+                if (i <= j)
+                {
+                    Swap<double>(ref koefs[i], ref koefs[j]);
+                    Swap<Game>(ref games[i], ref games[j]);
+                    i++;
+                    j--;
+                }
+            }
+            return i;
+        }
+
         public void QuickSort(double[] coefs, Game[] games, int left, int right)
         {
-            throw new NotImplementedException();
+            if (left < right)
+            {
+                int p = Partition(coefs, games, 0, coefs.Length - 1);
+
+                QuickSort(coefs, games, left, p - 1);
+                QuickSort(coefs, games, p, right);
+            }
         }
     }
 }
