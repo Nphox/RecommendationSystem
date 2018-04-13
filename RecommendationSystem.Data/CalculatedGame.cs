3fc2e7d7d38c7b988ec9851263fa602dd1909091
Dmitry Orlov <nphoxconnect@gmail.com>
2018-04-14 00:04:37 +0300

    add CalculatedGame

---
+++ RecommendationSystem.Data/CalculatedGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecommendationSystem.Data
+{
+    class CalculatedGame
+    {
+        private Game Game;
+        private double Coef;
+
+        public CalculatedGame(Game game, double coef)
+        {
+            Game = game;
+            Coef = coef;
+        }
+
+        public Game GetGame()
+        {
+            return Game;
+        }
+
+        public double GetCoef()
+        {
+            return Coef;
+        }
+    }
+}
